86cd2237c8dfaafed54ed81cb59516acc0fa226f
whfhdqkr <hyunsicktwitter0123456789@gmail.com>
2025-04-01 03:57:27 +0900

    el commit

---
+++ el/go text.js
@@ -0,0 +1,23 @@
+function MySliderBox1__init() {
+  var swiper = new Swiper('.my-slider-box-1 .swiper-container', {
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+    slidesPerView:1,
+    spaceBetween:0,
+    loop: true,
+    navigation: {
+      nextEl: '.my-slider-box-1 .my-slider-box-1__btn-right',
+      prevEl: '.my-slider-box-1 .my-slider-box-1__btn-left',
+    },
+  });
+}
+
+MySliderBox1__init();
+
+function MySliderBox2__init() {
+  var swiper = new Swiper('.my-slider-box-2 .swiper-container');
+}
+
+MySliderBox2__init();
